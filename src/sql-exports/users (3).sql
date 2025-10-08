ee9024329b37999c6f57dc942268fc42818ee7a3
putl0014 <putl0014@flinders.edu.au>
2025-10-08 14:35:26 +1030

    Updating db querys

--- src/sql-exports/users (3).sql
+++ src/sql-exports/users (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2025 at 01:55 PM
+-- Generation Time: Oct 08, 2025 at 06:04 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,9 +32,9 @@ CREATE TABLE `mailbox` (
   `id` int(45) NOT NULL,
   `Subject` varchar(255) NOT NULL,
   `message` varchar(255) NOT NULL,
-  `sentby` varchar(45) NOT NULL,
-  `sentto` varchar(45) NOT NULL,
-  `created` date NOT NULL
+  `sentby` varchar(60) NOT NULL,
+  `sentto` varchar(60) NOT NULL,
+  `created` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -42,7 +42,12 @@ CREATE TABLE `mailbox` (
 --
 
 INSERT INTO `mailbox` (`id`, `Subject`, `message`, `sentby`, `sentto`, `created`) VALUES
-(1, 'test', 'test', 'bob@flinders.edu.au', 'putl0014@flinders.edu.au', '2025-10-07');
+(1, 'test', 'test', 'bob@flinders.edu.au', 'putl0014@flinders.edu.au', '2025-10-07 12:30:00'),
+(2, 'Bob the tester', 'hello bob the tester', 'bob@flinders.edu.au', 'putl0014@flinders.edu.au', '2025-10-08 05:30:14'),
+(3, 'Testing Time Zone', 'it is currently 2:09pm', 'putl0014@flinders.edu.au', 'bob@flinders.edu.au', '2025-10-08 14:09:30'),
+(4, 'character limit', 'gfwhyuISKKKKKKKKKKKKKKKKKKKKKKKDHGAHSDFAHKGJFDSHKJFSHAGKJHGKJFSAHGSAFHGJASFHJGASFHGJFHJASGHGJSFAHGJFSAHGJSFAHGSAFGHJhjfjahkshjsfajhfsajhsafhjjfasjklfasjhlkfashjlfashjfashjfhsajhjfasjhkafsjhkfashjklfasljhlfasjlhafsjlhfasljhkafsljhkfashjlkhfjaslhjafshjklasf', 'putl0014@flinders.edu.au', 'bob@flinders.edu.au', '2025-10-08 14:17:27'),
+(5, 'Hello', 'testing Message', 'putl0014@flinders.edu.au', 'bob@flinders.edu.au', '2025-10-08 14:28:04'),
+(6, 'Hello', 'test 4', 'putl0014@flinders.edu.au', 'bob@flinders.edu.au', '2025-10-08 14:32:15');
 
 -- --------------------------------------------------------
 
@@ -100,16 +105,26 @@ CREATE TABLE `user_mailbox` (
   `id` int(45) NOT NULL,
   `user` varchar(45) NOT NULL,
   `mailbox` varchar(45) NOT NULL,
-  `message_id` int(45) NOT NULL
+  `mailbox_id` int(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_mailbox`
 --
 
-INSERT INTO `user_mailbox` (`id`, `user`, `mailbox`, `message_id`) VALUES
+INSERT INTO `user_mailbox` (`id`, `user`, `mailbox`, `mailbox_id`) VALUES
 (1, 'bob@flinders.edu.au', 'Out', 1),
-(2, 'putl0014@flinders.edu.au', 'In', 1);
+(2, 'putl0014@flinders.edu.au', 'In', 1),
+(3, 'putl0014@flinders.edu.au', 'In', 2),
+(4, 'bob@flinders.edu.au', 'Out', 2),
+(5, 'bob@flinders.edu.au', 'In', 3),
+(6, 'putl0014@flinders.edu.au', 'Out', 3),
+(7, 'bob@flinders.edu.au', 'In', 4),
+(8, 'putl0014@flinders.edu.au', 'Out', 4),
+(9, 'bob@flinders.edu.au', 'In', 5),
+(10, 'putl0014@flinders.edu.au', 'Out', 5),
+(11, 'bob@flinders.edu.au', 'In', 6),
+(12, 'putl0014@flinders.edu.au', 'Out', 6);
 
 --
 -- Indexes for dumped tables
@@ -141,7 +156,7 @@ ALTER TABLE `userdata`
 ALTER TABLE `user_mailbox`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `Message ID` (`message_id`);
+  ADD KEY `Message ID` (`mailbox_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -151,7 +166,7 @@ ALTER TABLE `user_mailbox`
 -- AUTO_INCREMENT for table `mailbox`
 --
 ALTER TABLE `mailbox`
-  MODIFY `id` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `messages`
@@ -169,7 +184,7 @@ ALTER TABLE `userdata`
 -- AUTO_INCREMENT for table `user_mailbox`
 --
 ALTER TABLE `user_mailbox`
-  MODIFY `id` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
@@ -179,7 +194,7 @@ ALTER TABLE `user_mailbox`
 -- Constraints for table `user_mailbox`
 --
 ALTER TABLE `user_mailbox`
-  ADD CONSTRAINT `Message ID` FOREIGN KEY (`message_id`) REFERENCES `mailbox` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `Message ID` FOREIGN KEY (`mailbox_id`) REFERENCES `mailbox` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
